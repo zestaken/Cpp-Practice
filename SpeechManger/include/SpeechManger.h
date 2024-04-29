fcb59d767a681890f51e27234702abd8beb74cc1
zestaken <1224649136@qq.com>
2024-04-29 10:02:44 +0800

    完成比赛主要流程

--- SpeechManger/include/SpeechManger.h
+++ SpeechManger/include/SpeechManger.h
@@ -16,7 +16,7 @@ private:
     vector<int> all_speaker_ids; //第一轮存储所有比赛选手的编号（顺序就是比赛抽签的顺序）
     vector<int> second_speaker_ids; //存储第二轮比赛选手的编号
     map<int, Speaker*> id_speaker; // 存储所有比赛选手的编号与比赛选手对象的对应关系
-
+    multimap<float, Speaker*> victor; //存储胜利者的信息(成绩和胜利选手)
     int index; //记录是第几轮比赛
 
 public:
@@ -26,6 +26,7 @@ public:
     void createSpeakers();
     void drawLots();
     void startCompetition();
+    void speechContest();
     SpeechManger(); //构造函数
     ~SpeechManger(); //析构函数
 };
--- SpeechManger/src/SpeechManger.cpp
+++ SpeechManger/src/SpeechManger.cpp
@@ -4,6 +4,10 @@
 #include "../include/SpeechManger.h"
 #include "../include/Speaker.h"
 #include <algorithm>
+#include <deque>
+#include <random>
+#include <functional>
+#include <ranges>
 
 void SpeechManger::showMenu() {
     // 提供初始界面和初始输入判断
@@ -42,19 +46,128 @@ void SpeechManger::createSpeakers() {
 }
 void SpeechManger::drawLots() {
     //打乱id列表的顺序，实现抽签的功能
-    cout << "第" << this->index << "轮比赛选手正在抽签......\n";
-    cout << "---------------------------------------" << endl;
-    random_shuffle(this->all_speaker_ids.begin(), this->all_speaker_ids.end());
-    int i = 1;
-    cout << "抽签结果如下：" << endl;
-    for (auto iter = this->all_speaker_ids.begin(); iter != this->all_speaker_ids.end(); iter++, i++) {
-        int id = *iter;
-        string name = this->id_speaker[id]->name;
-        cout << "No." << i << ": " << name << "; ";
-        if (i % 3 == 0)
-            cout << endl;
+
+    if (this->index == 1) {
+        cout << "第" << this->index << "轮比赛选手正在抽签......\n";
+        cout << "---------------------------------------" << endl;
+        random_shuffle(this->all_speaker_ids.begin(), this->all_speaker_ids.end());
+        int i = 1;
+        cout << "抽签结果如下：" << endl;
+        for (auto iter = this->all_speaker_ids.begin(); iter != this->all_speaker_ids.end(); iter++, i++) {
+            int id = *iter;
+            string name = this->id_speaker[id]->name;
+            cout << "No." << i << ": " << name << "; ";
+            if (i % 3 == 0)
+                cout << endl;
+        }
+    }
+    else {
+        cout << "第" << this->index << "轮比赛选手正在抽签......\n";
+        cout << "---------------------------------------" << endl;
+        random_shuffle(this->second_speaker_ids.begin(), this->second_speaker_ids.end());
+        int i = 1;
+        cout << "抽签结果如下：" << endl;
+        for (auto iter = this->second_speaker_ids.begin(); iter != this->second_speaker_ids.end(); iter++, i++) {
+            int id = *iter;
+            string name = this->id_speaker[id]->name;
+            cout << "No." << i << ": " << name << "; ";
+            if (i % 3 == 0)
+                cout << endl;
+        }
     }
 
+
+}
+
+void SpeechManger::speechContest() {
+    //如果是第一轮比赛，将选手分为每6人一组
+    //随机为每个选手打分，模拟裁判打分(裁判有10位，每个选手将有10个得分，去除最高分和最低分之后，得到的平均分为总分）
+    //如果是第一轮比赛，每个组的前三名晋级
+    // 如果是第二轮比赛，组的前三名赢得最终胜利
+
+    //构建c++11标准的随机数生成函数
+    random_device rd;  //用于产生随机数种子
+    auto gen = default_random_engine(rd()); //构建生成随机数的引擎
+    uniform_real_distribution<float> dis(60, 100); //指定随机数生成的分布
+    auto randFunc = bind(dis, gen);
+    vector<int> speakers;
+    if (this->index == 1) {
+        speakers = this->all_speaker_ids;
+        //因为key值是分数，有可能重复，所以使用允许key重复的multimap
+        multimap<float, int> score_id_1; //存储第一组每个选手的分数和编号的对应关系，可根据score的key值进行排序
+        multimap<float, int> score_id_2; //第二组
+        // 为每个参赛选手打分并存储
+        int speaker_num = 0;
+        for (auto iter = speakers.begin(); iter != speakers.end(); iter++, speaker_num++) {
+            deque<float> scores;
+            for (int i = 0; i < 10; i++) {
+                float score = randFunc(); //生成0-100之间0的浮点随机数
+                scores.push_back(score);
+            }
+            sort(scores.begin(), scores.end()); //进行分数从小到大排序
+            scores.pop_back();
+            scores.pop_front(); //去掉首尾最高和最低分
+            float avg_score = accumulate(scores.begin(), scores.end(), 0) / scores.size();
+            if (speaker_num < 6) {
+                //不同组的选手的成绩存入不同的容器中
+                score_id_1.insert(make_pair(avg_score, *iter));
+            } else {
+                score_id_2.insert(make_pair(avg_score, *iter));
+            }
+        }
+        //两组选手按照成绩排序，选出前三名
+        int j = 1;
+        cout << "第 1 组比赛成绩：" << endl;
+        //反向遍历map，从大到小获取成绩
+        for (auto & iter1 : std::ranges::reverse_view(score_id_1)) {
+            if (j <= 3)
+                this->second_speaker_ids.insert(this->second_speaker_ids.end(), iter1.second);
+            cout << "No." << j << ": 编号： " << iter1.second << ", 姓名： " << this->id_speaker[iter1.second]->name
+                 << ", 成绩： ";
+            cout << iter1.first << endl;
+            j++;
+        }
+        j = 1;
+        cout << "第 2 组比赛成绩：" << endl;
+        for (auto & iter2 : std::ranges::reverse_view(score_id_2)) {
+            if (j <= 3)
+                this->second_speaker_ids.insert(this->second_speaker_ids.end(), iter2.second);
+            cout << "No." << j << ": 编号： " << iter2.second << ", 姓名： " << this->id_speaker[iter2.second]->name << ", 成绩： ";
+            cout << iter2.first << endl;
+            j++;
+        }
+        }
+    else {
+        //第二轮比赛
+        speakers = this->second_speaker_ids;
+        multimap<float, int> score_id;
+        // 为每个参赛选手打分并存储
+        int speaker_num = 0;
+        for (auto iter = speakers.begin(); iter != speakers.end(); iter++, speaker_num++) {
+            deque<float> scores;
+            for (int i = 0; i < 10; i++) {
+                float score = randFunc(); //生成0-100之间0的浮点随机数
+                scores.push_back(score);
+            }
+            sort(scores.begin(), scores.end()); //进行分数从小到大排序
+            scores.pop_back();
+            scores.pop_front(); //去掉首尾最高和最低分
+            float avg_score = accumulate(scores.begin(), scores.end(), 0) / scores.size();
+            //将该选手的成绩插入score_id 映射中
+            score_id.insert(make_pair(avg_score, *iter));
+        }
+        int j = 1;
+        cout << "最终比赛成绩：" << endl;
+        //使用rbegin的反向遍历方式。
+        for (auto iter1 = score_id.rbegin(); iter1 != score_id.rend(); iter1++) {
+            if (j <= 3)
+                //存储最终胜利者信息
+                this->victor.insert(make_pair(iter1->first, id_speaker[iter1->second]));
+            cout << "No." << j << ": 编号： " << iter1->second << ", 姓名： " << this->id_speaker[iter1->second]->name << ", 成绩： ";
+            cout << iter1->first << endl;
+            j++;
+        }
+    }
 }
 
 void SpeechManger::startCompetition() {
@@ -64,12 +177,17 @@ void SpeechManger::startCompetition() {
     //1. 抽签/
     this->drawLots();
     //2. 比赛
+    this->speechContest();
     //3. 显示晋级结果
 
+    this->index++;
     //第二轮比赛
     //1.抽签
+    this->drawLots();
     //2. 比赛
+    this->speechContest();
     //3.显示最终结果
+
     //4. 保存结果到文件中
 }
 
